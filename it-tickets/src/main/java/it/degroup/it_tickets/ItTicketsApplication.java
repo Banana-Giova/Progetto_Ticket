9c5e97823df91914e3f8345bfdeda29c8780998e
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-07-15 17:02:43 +0200

    First upload

---
+++ it-tickets/src/main/java/it/degroup/it_tickets/ItTicketsApplication.java
@@ -0,0 +1,13 @@
+package it.degroup.it_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItTicketsApplication.class, args);
+	}
+
+}
---
+++ it-tickets/src/test/java/it/degroup/it_tickets/ItTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.degroup.it_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
