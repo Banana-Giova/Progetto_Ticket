c50699ebc0c00118dd91efde05b75cbebeab089c
gaiaflati <gaiaflati@live.it>
2025-07-16 17:43:11 +0200

    dao rimosso

--- it-tickets/src/main/java/it/degroup/it_tickets/dao/TicketDAO.java
+++
@@ -1,8 +0,0 @@
-package it.degroup.it_tickets.dao;
-
-import it.degroup.it_tickets.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketDAO extends JpaRepository<Ticket, Long> {
-
-}
