5e8c7e53e3af6a03d1d0dd321edaa251d7e4fb2d
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-07-17 12:06:20 +0200

    Configurazione Spring Security aggiornata

--- it-tickets/src/main/java/it/degroup/it_tickets/config/SecurityConfig.java
+++ it-tickets/src/main/java/it/degroup/it_tickets/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((authorize) -> authorize
-                    .requestMatchers("/login").permitAll()
+                    .requestMatchers("/", "/login", "/signup","/signup/**").permitAll()
                     .anyRequest().authenticated()
             );
 
@@ -24,6 +26,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
