3ec3009363320c2cab9d3551e60199e4ce82daa7
gaiaflati <gaiaflati@live.it>
2025-07-17 15:37:09 +0200

    aggiunta classe operationResult

---
+++ it-tickets/src/main/java/it/degroup/it_tickets/common/models/OperationResult.java
@@ -0,0 +1,27 @@
+package it.degroup.it_tickets.common.models;
+
+public class OperationResult<T> {
+
+    private static String OK_MESSAGE = "Operazione avvenuta con successo.";
+    private static String KO_MESSAGE = "Operazione fallita.";
+
+    private T data;
+    private String message;
+    private boolean ok;
+
+    public static <T> OperationResult<T> ok(T data, String message) {
+        OperationResult<T> result = new OperationResult<>();
+        result.ok = true;
+        result.data = data;
+        result.message = OperationResult.OK_MESSAGE;
+        return result;
+    }
+
+    public static <T> OperationResult<T> ko(String message) {
+        OperationResult<T> result = new OperationResult<>();
+        result.ok = false;
+        result.data = null;
+        result.message = OperationResult.KO_MESSAGE + " " + message;
+        return result;
+    }
+}
