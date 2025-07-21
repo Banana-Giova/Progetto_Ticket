9df9f2a18e118a700f6b33f56ac658c1d917e19c
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-07-21 11:00:44 +0200

    Aggiunto decoratore al Mapper

--- it-tickets/src/main/java/it/degroup/it_tickets/common/mappers/UserMapper.java
+++ it-tickets/src/main/java/it/degroup/it_tickets/common/mappers/UserMapper.java
@@ -3,11 +3,16 @@ package it.degroup.it_tickets.common.mappers;
 import it.degroup.it_tickets.entity.User;
 import it.degroup.it_tickets.presentation.responses.RegisterResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "id", source = "entity.id")
+    @Mapping(target = "email", source = "entity.email")
+    @Mapping(target = "name", source = "entity.name")
+    @Mapping(target = "surname", source = "entity.surname")
     RegisterResponse userToRegisterResponse(User user);
 }
